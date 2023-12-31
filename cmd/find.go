494f37e4468260f825730a8d8590255a274ab8f0
ravilock <ravi.me.professional@hotmail.com>
2023-12-31 01:10:13 -0300

    ref: cmd should use package 'dedup' to search for duplicate files

--- cmd/find.go
+++ cmd/find.go
@@ -1,14 +1,10 @@
 package cmd
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
-	"io"
 	"os"
-	"path"
 
-	"github.com/ravilock/dedupgo/internal"
+	"github.com/ravilock/dedupgo/dedup"
 	"github.com/spf13/cobra"
 )
 
@@ -18,7 +14,7 @@ func init() {
 	rootCmd.AddCommand(findCmd)
 
 	findCmd.Flags().BoolVarP(&recursive, "recursive", "r", false, "Recursively search through all subdirectories for duplicated files")
-	findCmd.Flags().IntVarP(&depth, "depth", "d", 0, "Define directory depth limit to search for duplicated files")
+	findCmd.Flags().IntVarP(&depth, "depth", "d", 0, "Define sub-directory depth limit to search for duplicated files")
 
 	findCmd.MarkFlagsMutuallyExclusive("recursive", "depth")
 }
@@ -31,61 +27,37 @@ var findCmd = &cobra.Command{
 	Short: "Find duplicate files in passed directory",
 	Long:  "Iterate through directory searching for duplicated files",
 	Args:  cobra.MaximumNArgs(1),
-	RunE:  find,
+	RunE:  findCommand,
 }
 
-type directory struct {
+type visitedDirectory struct {
 	path  string
 	depth int
 }
 
-func find(cmd *cobra.Command, args []string) error {
-	directoryQueue := &internal.Queue[directory]{}
-
+func findCommand(cmd *cobra.Command, args []string) error {
 	dirPath, err := directoryPath(args)
 	if err != nil {
 		return err
 	}
 
-	directoryQueue.Enqueue(directory{dirPath, 0})
-
-	files := make([]string, 0)
-
-	for directoryQueue.Length() != 0 {
-		currentDir, ok := directoryQueue.Dequeue()
-		if !ok || (depth != 0 && currentDir.depth == depth) {
-			break
-		}
-
-		dirEntries, err := os.ReadDir(currentDir.path)
-		if err != nil {
-			return err
-		}
-
-		for _, entry := range dirEntries {
-			entryPath := path.Join(currentDir.path, entry.Name())
-			if entry.IsDir() {
-				directoryQueue.Enqueue(directory{entryPath, currentDir.depth + 1})
-			} else {
-				files = append(files, entryPath)
-			}
-		}
-
-		if !recursive && depth == 0 {
-			break
-		}
+	findOptions := &dedup.FindOptions{}
+	if recursive {
+		findOptions.Mode = dedup.ModeRecursive
+	} else if depth != 0 {
+		findOptions.Mode = dedup.ModeDepth
+		findOptions.DepthLimit = depth
 	}
 
-	filesByBash, err := groupFilesByContentHash(files, dirPath)
+	filesByHash, err := dedup.Find(dirPath, findOptions)
 	if err != nil {
 		return err
 	}
-	removeNonDuplicatedFiles(filesByBash)
-	if len(filesByBash) == 0 {
+	if len(filesByHash) == 0 {
 		fmt.Println("No duplicates found")
 		return nil
 	}
-	for _, value := range filesByBash {
+	for _, value := range filesByHash {
 		fmt.Printf("%s are duplicate files\n", value)
 	}
 	return nil
@@ -96,69 +68,4 @@ func directoryPath(args []string) (string, error) {
 		return os.Getwd()
 	}
 	return args[0], nil
-}
-
-func getFiles(directory string) ([]os.DirEntry, error) {
-	return nil, nil
-}
-
-func filterFiles(entries []os.DirEntry) []os.DirEntry {
-	filtered := make([]os.DirEntry, 0)
-	for _, entry := range entries {
-		if entry.IsDir() {
-			continue
-		}
-		filtered = append(filtered, entry)
-	}
-	return filtered
-}
-
-func groupFilesByContentHash(files []string, dirPath string) (map[string][]string, error) {
-	contentHashToFileName := make(map[string][]string)
-	for _, file := range files {
-		hash2, err := hashFile(file, dirPath)
-		if err != nil {
-			return nil, err
-		}
-		hashString := hex.EncodeToString(hash2)
-		_, ok := contentHashToFileName[hashString]
-		if !ok {
-			contentHashToFileName[hashString] = []string{file}
-		} else {
-			contentHashToFileName[hashString] = append(contentHashToFileName[hashString], file)
-		}
-	}
-	return contentHashToFileName, nil
-}
-
-func hashFile(filePath string, dirPath string) ([]byte, error) {
-	buffer := make([]byte, 1024)
-	f, err := os.Open(filePath)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	hasher := sha256.New()
-	for {
-		n, err := f.Read(buffer)
-		if err != nil {
-			if err == io.EOF {
-				return hasher.Sum(nil), nil
-			}
-			return []byte{}, nil
-		}
-		if n == 1024 {
-			hasher.Write(buffer)
-		} else {
-			hasher.Write(buffer[:n])
-		}
-	}
-}
-
-func removeNonDuplicatedFiles(filesByHash map[string][]string) {
-	for key, value := range filesByHash {
-		if len(value) <= 1 {
-			delete(filesByHash, key)
-		}
-	}
 }
